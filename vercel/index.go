f28978284d7ad05484a4e1ca8f478cd39d4b521f
yqchilde <yqchilde@gmail.com>
2023-03-07 14:40:48 +0800

    feat: vercel/index.go

---
+++ vercel/index.go
@@ -0,0 +1,31 @@
+package vercel
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/proxy"
+)
+
+// Handler is the main entry point of the application. Think of it like the main() method
+func Handler(w http.ResponseWriter, r *http.Request) {
+	r.RequestURI = r.URL.String()
+	handler().ServeHTTP(w, r)
+}
+
+// building the fiber application
+func handler() http.HandlerFunc {
+	app := fiber.New()
+	app.All("/*", func(ctx *fiber.Ctx) error {
+		proxyUrl := fmt.Sprintf("https://%s/%s", os.Getenv("PROXY_DOMAIN"), ctx.Params("*", ""))
+		if err := proxy.Do(ctx, proxyUrl); err != nil {
+			return err
+		}
+		ctx.Response().Header.Del(fiber.HeaderServer)
+		return nil
+	})
+	return adaptor.FiberApp(app)
+}
